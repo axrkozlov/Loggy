d1b706a330f9363e85148e5a3ed2d717e2838a04
kozlov_43784 <FijuTine9980>
2020-06-17 12:16:05 +0600

    loggy uploader interface docs added

--- loggy/src/main/java/com/clawmarks/loggy/uploader/LoggyUploader.kt
+++ loggy/src/main/java/com/clawmarks/loggy/uploader/LoggyUploader.kt
@@ -6,6 +6,21 @@ import java.io.File
 /** interface LoggyUploader must be implemented as synchronous function, file must be sent one by one
 */
 interface LoggyUploader:KoinComponent {
+
+    /**
+     * Mime file type for an api
+     */
+    val mimeFileType
+        get() = "application/zlib"
+
+    /**
+     * Uploading files one by one, sending precess uses coroutine Dispatchers.IO synchronously
+     * therefore method must be implemented without background workers
+     */
     fun uploadSingleFile(file: File):Boolean
+
+    /**
+     * Uploading files can be cancelled when needed
+     */
     fun cancel()
 }
